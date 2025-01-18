f364eaece330e01ae49e91312575f0ecb948a6f5
LydiaAll <119404473+LydiaAll@users.noreply.github.com>
2025-01-18 18:18:00 +0100

    Delete backend directory

--- backend/block.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/blockNumber', (req, res) => {
-    res.json({ message: "Cette route fonctionne correctement !" });
-});
-
-module.exports = router;
